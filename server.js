a17ce1d2ffbce0bc0c14a9faccd3459a03c27ffa
ab siddik <absiddik96@gmail.com>
2020-05-12 22:25:55 +0600

    Setup and connect database

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const connectDB =  require('./config/db');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Connect database
+connectDB();
+
+// Test Route
 app.use('/', (req, res) => {
   res.json({mgs: "this is test"});
 });
 
 app.listen(port, () => {
-  console.log("Server is running on port:" + port);
+  console.log("Server is running on port: " + port);
 });
 
