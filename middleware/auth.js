cbd6b7a19abf995063cf5c1520f956931a29a756
ab siddik <absiddik96@gmail.com>
2020-05-13 21:30:47 +0600

    made auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const auth = (req, res, next) => {
+  // Get token from header
+  const token = req.header('x-auth-token');
+  
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+  
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+    res.json({ decode });
+  } catch (e) {
+    res.status(500).json({ errors: "Server error"})
+  }
+};
+
+module.exports = auth;
